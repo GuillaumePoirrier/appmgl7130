150bb29c2da6c8a4ccb4be3aeaf415a3f3d71512
Quentin Levasseur <quentin.levasseur@hei.yncrea.fr>
2019-02-17 02:10:38 -0500

    Add drawables and create Splash Screen Activity

---
+++ app/src/main/java/com/example/mgr850/applimgr7130/pages/activity_splash_screen.java
@@ -0,0 +1,15 @@
+package com.example.mgr850.applimgr7130.pages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mgr850.applimgr7130.R;
+
+public class activity_splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
