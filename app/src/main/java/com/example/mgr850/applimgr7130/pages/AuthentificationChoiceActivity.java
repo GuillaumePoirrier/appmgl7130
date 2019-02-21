d6440bf68f8eeac3697b1f9a85e3d78164874c3c
Quentin Levasseur <quentin.levasseur@hei.yncrea.fr>
2019-02-21 01:49:35 -0500

    Rensu executable avec un fonctionement basic et ajout d'une checkbox custom

--- app/src/main/java/com/example/mgr850/applimgr7130/pages/AuthentificationChoiceActivity.java
+++ app/src/main/java/com/example/mgr850/applimgr7130/pages/AuthentificationChoiceActivity.java
@@ -1,7 +1,11 @@
 package com.example.mgr850.applimgr7130.pages;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mgr850.applimgr7130.R;
 
@@ -11,5 +15,26 @@ public class AuthentificationChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_choice);
+
+        final Intent signInActivityIntent = new Intent(this, SignInActivity.class);
+        final Intent signUpActivityIntent = new Intent(this, SignUpActivity.class);
+
+        Button signInButton = (Button) findViewById(R.id.button_sign_in);
+        TextView signUpButton = (TextView) findViewById(R.id.textView_sign_up);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(signInActivityIntent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(signUpActivityIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/mgr850/applimgr7130/pages/SignInActivity.java
+++ app/src/main/java/com/example/mgr850/applimgr7130/pages/SignInActivity.java
@@ -2,10 +2,14 @@ package com.example.mgr850.applimgr7130.pages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 import com.example.mgr850.applimgr7130.R;
 
 public class SignInActivity extends AppCompatActivity {
+
+    RadioButton rememberMeRadioNutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
