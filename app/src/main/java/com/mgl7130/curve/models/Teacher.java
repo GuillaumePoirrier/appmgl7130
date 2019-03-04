c5adec2bbebc43074447041cff191977fe104629
Quentin Levasseur <quentin.levasseur@hei.yncrea.fr>
2019-03-04 18:08:55 -0500

    refractor student and teacher to extends generic user and fix graddle issue

--- app/src/main/java/com/mgl7130/curve/models/Teacher.java
+++ app/src/main/java/com/mgl7130/curve/models/Teacher.java
@@ -1,41 +1,9 @@
 package com.mgl7130.curve.models;
 
-import java.util.Date;
+public class Teacher extends User{
 
-public class Teacher {
-
-    private String fisrtName;
-    private String lastName;
-    private Date dateOfBirth;
-
-    public Teacher(String fisrtName, String lastName, Date dateOfBirth) {
-        this.fisrtName = fisrtName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getFisrtName() {
-        return fisrtName;
-    }
-
-    public void setFisrtName(String fisrtName) {
-        this.fisrtName = fisrtName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+    public Teacher(String fisrtName, String lastName) {
+        super(fisrtName, lastName);
     }
 
 }
