5d261985939b4fafc6e53c653808e9ac025e33eb
Guillaume Poirrier <guillaume.poirrier@hei.yncrea.fr>
2019-03-05 17:24:40 -0500

    <fix> : déplacement du packge profile_create dans le ui

--- app/src/main/java/com/mgl7130/curve/pages/teacher/ui/profile_create/TeacherProfileCreate.java
+++ app/src/main/java/com/mgl7130/curve/pages/teacher/ui/profile_create/TeacherProfileCreate.java
@@ -1,15 +1,12 @@
-package com.mgl7130.curve.pages.teacher.profile_create;
+package com.mgl7130.curve.pages.teacher.ui.profile_create;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.mgl7130.curve.R;
 
