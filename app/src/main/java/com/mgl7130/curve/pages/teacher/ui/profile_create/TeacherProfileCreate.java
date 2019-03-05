2898553ddd2a03611f86b47016b5b4b3dc5b04b3
Guillaume Poirrier <guillaume.poirrier@hei.yncrea.fr>
2019-03-05 17:36:03 -0500

    <feat> : ajout scrollview et bouton save profile teacher

--- app/src/main/java/com/mgl7130/curve/pages/teacher/ui/profile_create/TeacherProfileCreate.java
+++ app/src/main/java/com/mgl7130/curve/pages/teacher/ui/profile_create/TeacherProfileCreate.java
@@ -7,17 +7,68 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mgl7130.curve.R;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class TeacherProfileCreate extends Fragment {
+
+    private EditText teacherBirthDate;
+    private Calendar calendar ;
+    private int Year, Month, Day ;
+    private DatePickerDialog datePickerDialog ;
+    private Button saveProfileTeacher;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.teacher_profile_form, container, false);
+
+
+        calendar = Calendar.getInstance();
+        Year = calendar.get(Calendar.YEAR) ;
+        Month = calendar.get(Calendar.MONTH);
+        Day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        teacherBirthDate = view.findViewById(R.id.teacher_birth_date);
+
+        teacherBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                        String dateString = new SimpleDateFormat("dd/MM/yyyy").format(new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime());
+                        teacherBirthDate.setText(dateString);
+                    }
+                };
+                datePickerDialog = DatePickerDialog.newInstance(onDateSetListener, Year, Month, Day);
+                datePickerDialog.setAccentColor(getResources().getColor(R.color.color_blue));
+                datePickerDialog.setTitle(getString(R.string.set_class_date));
+                datePickerDialog.show(getActivity().getFragmentManager(), "DatePickerDialog");
+            }
+        });
+
+        //TODO : Save profile
+
+
+
+
+
+
+
+
+
         return view;
+
     }
 
     public static Fragment newInstance() {
