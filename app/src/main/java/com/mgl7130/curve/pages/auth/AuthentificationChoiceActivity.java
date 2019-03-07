88ce6f9781197d57cf297532437368537fc9381a
Quentin Levasseur <quentin.levasseur@hei.yncrea.fr>
2019-03-07 02:43:10 -0500

    refractor to use Butterknife

--- app/src/main/java/com/mgl7130/curve/pages/auth/AuthentificationChoiceActivity.java
+++ app/src/main/java/com/mgl7130/curve/pages/auth/AuthentificationChoiceActivity.java
@@ -10,39 +10,37 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.mgl7130.curve.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AuthentificationChoiceActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    Intent signInActivityIntent;
+    Intent signUpActivityIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_choice);
+        ButterKnife.bind(this);
 
         mAuth = FirebaseAuth.getInstance();
 
-        final Intent signInActivityIntent = new Intent(this, SignInActivity.class);
-        final Intent signUpActivityIntent = new Intent(this, SignUpActivity.class);
-
-        Button signInButton = (Button) findViewById(R.id.button_sign_in);
-        TextView signUpButton = (TextView) findViewById(R.id.textView_sign_up);
-
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(signInActivityIntent);
-            }
-        });
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(signUpActivityIntent);
-            }
-        });
+        signInActivityIntent = new Intent(this, SignInActivity.class);
+        signUpActivityIntent = new Intent(this, SignUpActivity.class);
 
     }
 
+    @OnClick(R.id.button_sign_in)
+    public void onSignInClicked(View view) {
+        startActivity(signInActivityIntent);
+    }
+
+    @OnClick(R.id.textView_sign_up)
+    public void onSignUpClicked(View view) {
+        startActivity(signUpActivityIntent);
+    }
 
     //    prevent user to go back to splash screen
     @Override
