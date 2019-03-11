cc2079713cccbbc278e902620d6207effcd0cfc6
Romain <romain.gouraud@hotmail.com>
2019-03-11 17:47:43 -0400

    Recycler view pour Student

--- app/src/main/java/com/mgl7130/curve/models/Student.java
+++ app/src/main/java/com/mgl7130/curve/models/Student.java
@@ -1,6 +1,6 @@
 package com.mgl7130.curve.models;
 
-import java.util.Date;
+import android.graphics.drawable.Drawable;
 
 public class Student extends User{
 
@@ -12,6 +12,12 @@ public class Student extends User{
 
     public Student getStudentFromId(String studentId){
         //TODO get Student object corresponding to String id
+        return null;
+    }
+
+    // IMPORTANT : À définir !!
+
+    public Drawable getImgStudent (){
         return null;
     }
 
--- app/src/main/java/com/mgl7130/curve/pages/student/list/StudentClassRecyclerFragment.java
+++ app/src/main/java/com/mgl7130/curve/pages/student/list/StudentClassRecyclerFragment.java
@@ -1,10 +1,9 @@
-package com.mgl7130.curve.pages.teacher.ui.classes.list;
+package com.mgl7130.curve.pages.student.list;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -18,49 +17,54 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.mgl7130.curve.R;
-import com.mgl7130.curve.pages.teacher.ui.classes.create.TeacherClassFormActivity;
-import com.mgl7130.curve.pages.teacher.ui.classes.detail.TeacherClassDetailActivity;
+import com.mgl7130.curve.models.Student;
+import com.mgl7130.curve.pages.student.detail.StudentClassDetailActivity;
+import com.mgl7130.curve.pages.teacher.ui.classes.list.TeacherClassAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class TeacherClassRecyclerFragment extends Fragment implements
-        TeacherClassAdapter.OnClassSelectedListener {
+import static com.mgl7130.curve.R.id.recycler_view_student;
 
-    public static final String TAG = "TeacherClassRecyclerFragment";
-    public static final int LIMIT = 50;
+public class StudentClassRecyclerFragment extends Fragment implements StudentClassAdapter.OnClassSelectedListener {
 
-    @BindView(R.id.recycler_view)
-    RecyclerView mClassRecycler;
+        public static final String TAG = "StudentClassRecyclerFragment";
+        public static final int LIMIT = 50;
 
-    @BindView(R.id.viewEmpty)
-    ViewGroup mEmptyView;
+        @BindView(recycler_view_student)
+        RecyclerView mClassRecycler;
 
-    private FirebaseFirestore mFirestore;
-    private FirebaseAuth mAuth;
-    private Query mQuery;
+        @BindView(R.id.viewEmpty_student)
+        ViewGroup mEmptyView;
 
-    private TeacherClassAdapter mAdapter;
+        private FirebaseFirestore mFirestore;
+        private FirebaseAuth mAuth;
+        private Query mQuery;
+
+        private StudentClassAdapter mAdapter;
+
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.teacher_class_recycler_view_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.student_class_recycler_view_fragment, container, false);
         ButterKnife.bind(this, view);
 
         //Firestore
         mFirestore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
+        // ATTENTION STUDENT ID !!!!!!
+
         //Get ${LIMIT} class where teacherId == user id
         mQuery = mFirestore.collection("classes")
-                .whereEqualTo("teacher_id", mAuth.getCurrentUser().getUid())
+                .whereEqualTo("student_id", mAuth.getCurrentUser().getUid())
                 .orderBy("date", Query.Direction.ASCENDING)
                 .limit(LIMIT);
 
         //RecyclerView
-        mAdapter = new TeacherClassAdapter(mQuery, this) {
+        mAdapter = new StudentClassAdapter(mQuery, this) {
             @Override
             protected void onDataChanged() {
                 mClassRecycler.setVisibility(View.VISIBLE);
@@ -70,20 +74,16 @@ public class TeacherClassRecyclerFragment extends Fragment implements
             @Override
             protected void onError(FirebaseFirestoreException e) {
                 // Show a snackbar on errors
-                Log.e(TAG,e.toString());
+                Log.e(TAG, e.toString());
             }
+
         };
 
         mClassRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
         mClassRecycler.setAdapter(mAdapter);
 
         return view;
-    }
 
-    @OnClick(R.id.fab_add_class)
-    public void onAddClassFABClicked(View view){
-        Intent intent = new Intent(getActivity(), TeacherClassFormActivity.class);
-        startActivity(intent);
     }
 
     @Override
@@ -106,14 +106,13 @@ public class TeacherClassRecyclerFragment extends Fragment implements
     @Override
     public void onClassSelected(DocumentSnapshot cours) {
         // Go to the details page for the selected restaurant
-        Intent intent = new Intent(getActivity(), TeacherClassDetailActivity.class);
-        intent.putExtra(TeacherClassDetailActivity.KEY_CLASS_ID, cours.getId());
+        Intent intent = new Intent(getActivity(), StudentClassDetailActivity.class);
+        intent.putExtra(StudentClassDetailActivity.KEY_CLASS_ID, cours.getId());
 
         startActivity(intent);
     }
 
     public static Fragment newInstance(){
-        return new TeacherClassRecyclerFragment();
+        return new StudentClassRecyclerFragment();
     }
-
 }
