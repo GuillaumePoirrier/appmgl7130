a8e13d2a7a048b771dc307105e1f40ec9c611947
Guillaume POIRRIER <guillaume.poirrier@hei.yncrea.fr>
2019-03-12 01:49:06 -0400

    <fix> : remise a niveau du profil teacher, correction fautes, refactoring et ajout de la description

--- app/src/main/java/com/mgl7130/curve/models/Teacher.java
+++ app/src/main/java/com/mgl7130/curve/models/Teacher.java
@@ -5,8 +5,8 @@ import com.google.firebase.Timestamp;
 public class Teacher extends User{
 
 
-    public Teacher(String fisrtName, String lastName) {
-        super(fisrtName, lastName);
+    public Teacher(String firstName, String lastName) {
+        super(firstName, lastName);
 
     }
 
