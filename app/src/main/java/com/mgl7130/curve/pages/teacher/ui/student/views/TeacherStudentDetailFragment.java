669fec3ea9cde0b9b3d028a0af358a659b6935de
Quentin Levasseur <quentin.levasseur@hei.yncrea.fr>
2019-04-16 20:26:10 -0400

    MVVM teacher student detail

--- app/src/main/java/com/mgl7130/curve/pages/teacher/ui/student/views/TeacherStudentDetailFragment.java
+++ app/src/main/java/com/mgl7130/curve/pages/teacher/ui/student/views/TeacherStudentDetailFragment.java
@@ -1,5 +1,7 @@
 package com.mgl7130.curve.pages.teacher.ui.student.views;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -22,8 +24,11 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.storage.FirebaseStorage;
 import com.mgl7130.curve.R;
+import com.mgl7130.curve.databinding.FragmentTeacherStudentDetailBinding;
 import com.mgl7130.curve.models.Cours;
 import com.mgl7130.curve.models.Student;
+import com.mgl7130.curve.pages.teacher.ui.student.viewmodels.TeacherStudentDetailViewModel;
+import com.mgl7130.curve.util.DateTimeUtil;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
@@ -36,25 +41,9 @@ public class TeacherStudentDetailFragment extends Fragment {
     public static final String TAG = "TeacherClassDetailAct";
     public static final String KEY_STUDENT_ID = "key_student_id";
 
-    @BindView(R.id.iv_teacher_student_detail_student_image)
-    ImageView subjectImage;
-
-    @BindView(R.id.tv_teacher_student_detail_name)
-    TextView name;
-
-    @BindView(R.id.tv_teacher_student_detail_age)
-    TextView age;
-
-    @BindView(R.id.tv_teacher_student_detail_description_label)
-    TextView description;
-
-
-    private FirebaseFirestore mFirestore;
-    private DocumentReference mStudentRef;
-    private DocumentReference mClassRef;
-    private ListenerRegistration mClassRegistration;
+    private FragmentTeacherStudentDetailBinding mBinding;
+    private TeacherStudentDetailViewModel mViewModel;
     private String classId;
-    private String studentId;
 
     public static Fragment newInstance() {
         return new TeacherStudentDetailFragment();
@@ -62,82 +51,24 @@ public class TeacherStudentDetailFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_teacher_student_detail, container, false);
-        ButterKnife.bind(this, view);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_teacher_student_detail, container, false);
 
         classId = getArguments().getString(KEY_STUDENT_ID);
         if (classId == null) {
             throw new IllegalArgumentException("Must pass extra " + KEY_STUDENT_ID);
         }
 
-        //init Firestore
-        mFirestore = FirebaseFirestore.getInstance();
-
-//
-        mClassRef = mFirestore.collection("classes").document(classId);
-
-        return view;
+        return mBinding.getRoot();
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(TeacherStudentDetailViewModel.class);
+        mBinding.setLifecycleOwner(this);
+        mViewModel.setId(classId);
 
-        mClassRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(DocumentSnapshot snapshot, FirebaseFirestoreException e) {
-                if (e != null) {
-                    Log.w(TAG, "class:onEvent", e);
-                    return;
-                }
-                onClassLoaded(snapshot.toObject(Cours.class));
-            }
-        });
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        if (mClassRegistration != null) {
-            mClassRegistration.remove();
-            mClassRegistration = null;
-        }
-    }
-
-    private void onClassLoaded(final Cours cours) {
-        if (cours.getStudent_id() != null) {
-            mStudentRef = mFirestore.collection("users").document(cours.getStudent_id());
-            mStudentRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-                @Override
-                public void onEvent(DocumentSnapshot snapshot, FirebaseFirestoreException e) {
-                    if (e != null) {
-                        Log.w(TAG, "class:onEvent", e);
-                        return;
-                    }
-                    onStudentLoaded(snapshot.toObject(Student.class), cours.getStudent_id());
-                }
-            });
-        }
-    }
-
-    private void onStudentLoaded(Student student, String studentId) {
-        if (student != null) {
-            String fullname = student.getFirstName() + " " + student.getLastName();
-            name.setText(fullname);
-            if (student.hasBirthDate()) {
-                age.setText(new SimpleDateFormat("dd MMMM yyyy", Locale.CANADA_FRENCH).format(student.getBirthDate().toDate()));
-            }
-            if (student.hasDescription()) description.setText(student.getDescription());
-            FirebaseStorage.getInstance().getReference().child("curve/" + studentId + ".jpg")
-                    .getBytes(100000).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-                @Override
-                public void onSuccess(byte[] bytes) {
-                    Bitmap profilePicture = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                    subjectImage.setImageBitmap(profilePicture);
-                }
-            });
-        }
+        mViewModel.getStudent().observe(this, student -> mBinding.setStudent(student));
     }
 
 }
--- app/src/test/java/com/mgl7130/curve/util/DateTimeUtilTest.java
+++ app/src/test/java/com/mgl7130/curve/util/DateTimeUtilTest.java
@@ -12,6 +12,77 @@ import static org.junit.Assert.*;
 
 public class DateTimeUtilTest {
 
+    @Test
+    public void getTimefromTimestamp() {
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date);
+        String normalResult = new SimpleDateFormat("HH:mm", Locale.CANADA_FRENCH).format(date);
+
+        String result = DateTimeUtil.getTime(timestamp);
+
+        assertNotNull(result);
+        assertEquals(result, normalResult);
+    }
+
+    @Test
+    public void getTimefromNullTimestamp() {
+        String result = DateTimeUtil.getTime(null);
+        assertNull(result);
+    }
+
+    @Test
+    public void getDayFromTimestamp() {
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date);
+        String normalResult = new SimpleDateFormat("dd", Locale.CANADA_FRENCH).format(date);
+
+        String result = DateTimeUtil.getDay(timestamp);
+
+        assertNotNull(result);
+        assertEquals(result, normalResult);
+    }
+
+    @Test
+    public void getDayFromNullTimestamp() {
+        String result = DateTimeUtil.getDay(null);
+        assertNull(result);
+    }
+
+    @Test
+    public void getMonth() {
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date);
+        String normalResult = new SimpleDateFormat("MMM", Locale.CANADA_FRENCH).format(date);
+
+        String result = DateTimeUtil.getMonth(timestamp);
+
+        assertNotNull(result);
+        assertEquals(result, normalResult);
+    }
+
+    @Test
+    public void getMonthFromNullTimestamp() {
+        String result = DateTimeUtil.getMonth(null);
+        assertNull(result);
+    }
+
+    @Test
+    public void getYearFromTimestamp() {
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date);
+        String normalResult = new SimpleDateFormat("yyyy", Locale.CANADA_FRENCH).format(date);
+
+        String result = DateTimeUtil.getYear(timestamp);
+
+        assertNotNull(result);
+        assertEquals(result, normalResult);
+    }
+
+    @Test
+    public void getYearFromNullTimestamp() {
+        String result = DateTimeUtil.getYear(null);
+        assertNull(result);
+    }
 
     @Test
     public void getDateString() {
@@ -19,7 +90,7 @@ public class DateTimeUtilTest {
         Timestamp timestamp = new Timestamp(date);
         String normalResult = new SimpleDateFormat("dd MMMM", Locale.CANADA_FRENCH).format(date);
 
-        String result = CoursUtil.getDateString(timestamp);
+        String result = DateTimeUtil.getDateString(timestamp);
 
         assertNotNull(result);
         assertEquals(result, normalResult);
